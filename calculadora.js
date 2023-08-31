20f77b7c27b3da27bb77a3d6c0419d0562f196bc
Bell <133970312+bedobell@users.noreply.github.com>
2023-08-31 09:13:26 -0300

    mais alterações

--- calculadora.js
+++ calculadora.js
@@ -2,7 +2,10 @@ var somar = require("./modulos/somar");
 var media = require("./modulos/media");
 var divisao = require("./modulos/divisao");
 var multi = require("./modulos/multi");
-console.log(somar(20,90));
-console.log(media(20,90));
-console.log(multi(20,90));
-console.log(divisao(20,90));
+var mensagem = require("./modulos/mensagem");
+
+console.log(`A soma entre 20 + 90 é: ${somar(20,90)}`);
+console.log(`A média entre 20 + 90 é: ${media(20,90)}`);
+console.log(`A multiplicação entre 20 + 90 é: ${multi(20,90)}`);
+console.log(`A divisão entre 20 + 90 é: ${divisao(20,90)}`);
+console.log(mensagem("Bell"))
---
+++ modulos/mensagem.js
@@ -0,0 +1,4 @@
+var mensagem = function (nome) {
+  return "Esta calculadora é muito foda mas a Taylor Swift é mais ainda \n e foi feita por: "+nome
+}; 
+module.exports = mensagem;
